29e45cd8ddccd2fb59792b61b1b9ee630f601e4c
Soichiro-Yoshimura <yoshimura@soichiro.org>
2016-10-20 13:37:12 +0900

    First commit

---
+++ app/src/main/java/com/example/android/fragments/HeadlinesFragment.java
@@ -0,0 +1,34 @@
+package com.example.android.fragments;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class HeadlinesFragment extends ListFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // We need to use a different list item layout for devices older than Honeycomb
+        int layout = Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ?
+                android.R.layout.simple_list_item_activated_1 : android.R.layout.simple_list_item_1;
+
+        // Create an array adapter for the list view, using the Ipsum headlines array
+        setListAdapter(new ArrayAdapter<String>(getActivity(), layout, Ipsum.Headlines));
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // When in two-pane layout, set the listview to highlight the selected list item
+        // (We do this during onStart because at the point the listview is available.)
+        if (getFragmentManager().findFragmentById(R.id.article_fragment) != null) {
+            getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        }
+    }
+
+}
